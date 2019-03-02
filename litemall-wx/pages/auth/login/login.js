c9821adc5e028bf6eac69d362580db4220181a1f
miko <136057505@qq.com>
2019-03-03 07:29:48 +0800

    Initial commit

---
+++ litemall-wx/pages/auth/login/login.js
@@ -0,0 +1,55 @@
+var api = require('../../../config/api.js');
+var util = require('../../../utils/util.js');
+var user = require('../../../utils/user.js');
+
+var app = getApp();
+Page({
+  onLoad: function(options) {
+    // 页面初始化 options为页面跳转所带来的参数
+    // 页面渲染完成
+
+  },
+  onReady: function() {
+
+  },
+  onShow: function() {
+    // 页面显示
+  },
+  onHide: function() {
+    // 页面隐藏
+
+  },
+  onUnload: function() {
+    // 页面关闭
+
+  },
+  wxLogin: function(e) {
+    if (e.detail.userInfo == undefined) {
+      console.log('>>>')
+      app.globalData.hasLogin = false;
+      util.showErrorToast('微信登录失败');
+      return;
+    }
+
+    user.checkLogin().catch(() => {
+
+      user.loginByWeixin(e.detail.userInfo).then(res => {
+        app.globalData.hasLogin = true;
+
+        wx.navigateBack({
+          delta: 1
+        })
+      }).catch((err) => {
+        console.log('1111')
+        app.globalData.hasLogin = false;
+        util.showErrorToast('微信登录失败');
+      });
+
+    });
+  },
+  accountLogin: function() {
+    wx.navigateTo({
+      url: "/pages/auth/accountLogin/accountLogin"
+    });
+  }
+})
